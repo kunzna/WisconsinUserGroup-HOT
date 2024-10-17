a1340f798e952563d6e17c4f45c91d53d721c8e1
kunzna <nathan.kunz@gmail.com>
2024-10-17 15:20:14 -0500

    Update setuploggen.sh

--- setuploggen.sh
+++ setuploggen.sh
@@ -2,10 +2,12 @@ kubectl delete ns easytrade
 kubectl create namespace easytrade
 
 cp /home/dtu_training/easytrade/kubernetes-manifests/ingress.yaml /home/dtu_training/WisconsinUserGroup-HOT/deployment/ingress.yaml
+
 kubectl -n easytrade apply -f ./deployment
 
 kubectl rollout status deployment -n easytrade --timeout=300s
 
+kubectl delete ns log-generator
 kubectl create namespace log-generator
 
 # cd /home/dtu_training/WisconsinUserGroup-HOT
@@ -15,5 +17,3 @@ kubectl apply -f LogGenerator.yaml -n log-generator
 kubectl rollout status deployment log-generator -n log-generator --timeout=90s
 
 kubectl logs -f -n log-generator -l app=log-generator
-
-
